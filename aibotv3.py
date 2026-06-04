f4570e291312884c2496a302911125be31562732
BenG11-cybersec <horvath.benedek.s@gmail.com>
2026-06-04 13:12:53 +0200

    some fixes

--- aibotv3.py
+++ aibotv3.py
@@ -61,8 +61,8 @@ except ImportError:
 #  KONFIGURÁCIÓ
 # ═══════════════════════════════════════════════════════
 
-DISCORD_WEBHOOK_BULL = "https://discord.com/api/webhooks/1511371177669431447/CHdWK10D8Ye8K4mdguGAlx6wSTdyGBxRJScZWWomti6lWYkSMRpte3BGy3wpCFXZwn4O"
-DISCORD_WEBHOOK_BEAR = "https://discord.com/api/webhooks/1511721351050956993/sKbh-BUzvSF6nmj77kGyeJuev_4hzR6C3gBZ0rr__1PqyMSzo_qHdBe-eaGRrOez5rXi"
+DISCORD_WEBHOOK_BULL = "PASTE YOUR DISCORD WEBHOOK FOR BULLISH MOVEMENTS HERE!!!"
+DISCORD_WEBHOOK_BEAR = "PASTE YOUR DISCORD WEBHOOK FOR BEARISH MOVEMENTS HERE!!!!"
 
 WATCHLIST = ["NVDA", "DELL", "PANW", "RHM.DE", "NFLX", "AAPL", "MSFT", "NOW", "CVS","VCEL", "AXON", "MC","AIR", "COHR", "BLK", "BYD","CVS", "LCID", "DOCS", "S", "VST", "KRNT"]
 
